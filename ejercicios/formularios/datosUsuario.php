1e47bd14b80e848b559d7bd7307c800fef2f72c5
Elena G <elena.guzbla@gmail.com>
2017-11-28 16:29:16 +0100

    datos de usuario

--- ejercicios/formularios/datosUsuario.php
+++ ejercicios/formularios/datosUsuario.php
@@ -3,9 +3,40 @@
 include "../plantillas/print_html.php";
 
 // Rescatando datos:
+$nombre = $_POST["nombre"];
+$apellidos = $_POST["apellidos"];
+$username = $_POST["username"];
+$passwd = $_POST["passwd"];
+$pass_rpt = $_POST["passwd_repeat"];
+$email = $_POST["email"];
+$fecha = $_POST["fecha"];
+$ciudad = $_POST["ciudad"];
+$pais = $_POST["pais"];
+$acepta = isset($_POST["acepta"]);
+$frec = $_POST["frec"];
 
 // Mostrando HTML:
-howHTMLHeader("Cálculo de hipotenusa");
+howHTMLHeader("Recogiendo datos de usuario");
+
+	print "El usuario " .$nombre. " y " .$apellidos. ", nacido en " .$fecha. ", con usuario " .$username. " y email " .$email. " ";
+
+	if ($acepta)
+		print "acepta ";
+	else
+		print "no acepta ";
+
+	print "los términos y condiciones de uso de rolForevah.</p>";
+
+	print "<p> Se registra desde " .$ciudad. ", en " .$pais. "</p>";
+
+	print "<p> Su contraseña y repetición de contraseña ";
+
+	if ($passwd == $passwd_repeat)
+		print "<p>coinciden</p>";
+	else
+		print "<p>no coinciden</p>";
+
+	print "<p> Ha decidido recibir novedades cada " .$frec. "</p>";
 
 showHTMLFooter();
 
